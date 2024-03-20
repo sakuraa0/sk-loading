be53474a13929035c0c4e6e6b6b18605cc00b59b
sakuraa0#0001 <69822832+SAKURA-8872@users.noreply.github.com>
2024-03-20 01:49:56 +0000

    Loading Screen from my city!

---
+++ assets/scripts/config.js
@@ -0,0 +1,30 @@
+Config = {}; // Don't touch
+
+Config.ServerIP = "";
+
+// Social media buttons on the left side
+Config.Socials = [
+    {name: "discord", label: "Discord", description: "carrega aqui para te juntares ao nosso Discord!", icon: "assets/media/icons/discord.png", link: "https://discord.gg/RKxFvz9RN5"},
+    {name: "instagram", label: "Instagram", description: "Brevemente!", icon: "assets/media/icons/tiktok.png", link: ""},
+//    {name: "tebex", label: "Donations", description: "For donations, feel free to look at the room - #Donations at Discord.", icon: "assets/media/icons/tebex.png", link: "https://discord.gg/NHq8bP3TVy"},
+];
+
+Config.HideoverlayKeybind = 112 // JS key code https://keycode.info
+Config.CustomBindText = "F1"; // leave as "" if you don't want the bind text in html to be statically set
+
+// Staff list
+Config.Staff = [
+    {name: "SÄÄKÜÜRÄÄ#8872", description: "Owner & Dev", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/965285436006232195/1091084574483808396/filled-logo2.png"},
+    {name: "JoscaPT#9453", description: "Owner", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/965285436006232195/1091084574483808396/filled-logo2.png"},
+    {name: "Andre_Ribeiro7#5839", description: "Owner & Dev & Designer", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/965285436006232195/1091084574483808396/filled-logo2.png"},
+    {name: "Jop1n#9411", description: "Administrador", color: "#ff0000", image: "https://cdn.discordapp.com/attachments/965285436006232195/1091084574483808396/filled-logo2.png"},
+];
+
+// Categories
+Config.Categories = [
+    {label: "Redes Sociais", default: true},
+    {label: "Staff", default: false}
+];
+
+// Music
+Config.Song = "song.mp3";
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+description 'Alma loading'
+version '0.1'
+
+files {
+    '*.html',
+    'assets/**/*.*',
+    'assets/**/**/*.*'
+}
+
+
+
+--loadscreen_manual_shutdown "yes"
+loadscreen 'index.html'
